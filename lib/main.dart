580f0c321b6464705e257078e14e8b05008783a1
Eleandro Duzentos <eleandro@inbox.ru>
2020-05-19 02:13:20 +0100

    Added REST API fetch data functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,50 +14,70 @@ class InfiniteScroll extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Infinite Scroll'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({
+    Key key,
+  }) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Map<String, dynamic>> _posts = [
-    {
-      'id': 1,
-      'title': 'Foo',
-      'body': 'Lorem',
-    },
-    {
-      'id': 2,
-      'title': 'Bar',
-      'body': 'Ipsum',
-    },
-  ];
+  final _http = Dio(
+    BaseOptions(baseUrl: 'https://reqres.in/'),
+  );
+
+  final List<Map<String, dynamic>> _users = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Users: ${_users.length}'),
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          final _post = _posts[index];
+          final _post = _users[index];
 
           return ListTile(
-            title: Text(_post['title']),
-            subtitle: Text(_post['body']),
+            title: Text(
+              _post['name'],
+            ),
+            subtitle: Text(
+              _post['email'],
+            ),
           );
         },
-        itemCount: _posts.length,
+        itemCount: _users.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          _fetchData(page: 1, limit: 10);
+        },
       ),
     );
+  }
+
+  _fetchData({int page, int limit}) {
+    _http.get('/api/users?page=$page&per_page=$limit').then((response) {
+      final _fetchedUsers = (response.data['data'] as List)
+        .map((_user) => {
+          'id': _user['id'],
+          'email': _user['email'],
+          'name': '${_user['first_name']} ${_user['last_name']}',
+        })
+        .toList();
+
+      setState(() {
+        _users.addAll(_fetchedUsers);
+      });
+    });
   }
 }
